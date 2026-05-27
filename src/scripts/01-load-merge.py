ccad5c3bb1786a39be743f3307140568f131c011
SebastianSchnitzer11825345 <e11825345@student.tuwien.ac.at>
2026-05-27 14:50:22 +0200

    Update load-merge with DBrepo api information

--- src/scripts/01-load-merge.py
+++ src/scripts/01-load-merge.py
@@ -7,12 +7,15 @@ No local CSV files needed.
 from pathlib import Path
 import pandas as pd
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # --- Config ---
-API_BASE = "https://..."   # fill in when available
-DB_ID    = 123             # fill in when available
-USERNAME = "..."
-PASSWORD = "..."
+API_BASE = "https://test.dbrepo.tuwien.ac.at/api/v1"   # fill in when available
+DB_ID    = "3d81c073-e5fd-49b9-9536-b75ed490ca3e"             # fill in when available
+USERNAME = os.getenv("DBREPO_USER")
+PASSWORD = os.getenv("DBREPO_PASSWORD")
 
 REPO_ROOT = Path(__file__).resolve().parents[2]
 INTERIM   = REPO_ROOT / "data" / "interim"
